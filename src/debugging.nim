bf5788581f30df833728b2d272fd64565028801e
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2023-10-17 22:52:14 +0900

    APILoggingをテンプレートで実装

--- src/debugging.nim
+++ src/debugging.nim
@@ -6,10 +6,12 @@ import
 const isDebug {.booldefine.} : bool = true
 
 when isDebug:
+  # ログを保存するディレクトリがないとき作る
   if not dirExists(getAppDir() / "log"):
-      createDir(getAppDir() / "log")
+    createDir(getAppDir() / "log")
 
-  proc debugLogging*(level, procName, message:string): void =
+  # ログを出力
+  proc DebugLogging*(level, procName, message:string): void =
     let nowTime = now().format("yyyy-MM-dd HH:mm:ss")
     let log = &"[{level}]:{procName} -> {message} -> " & nowTime
     echo log
@@ -17,6 +19,15 @@ when isDebug:
     f.writeLine(log)
     f.close()
 
+  # apiにアクセスした時のログを出力
+  template APILogging*(reqMethod, path, message:string, body:untyped): untyped =
+    DebugLogging(reqMethod, path, "Http"&reqMethod&" comes.")
+    body
+    DebugLogging("SUCCESS", path, message)
+
 else:
-  proc debugLogging*(level, text:string): void =
+  proc DebugLogging*(level, text:string): void =
     discard
+
+  template APILogging*(reqMethod, path, message:string, body:untyped): untyped =
+    body
