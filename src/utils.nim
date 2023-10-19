d9883ae290044e7e859ca32b7460c3c16a030006
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2023-10-19 19:20:43 +0900

    middlewareにjwtを検証してidを取り出す処理を実装

--- src/utils.nim
+++ src/utils.nim
@@ -2,6 +2,7 @@ import
   std/json,
   std/os,
   std/strformat,
+  std/tables,
   std/times,
   jwt,
   ./debugging
@@ -9,10 +10,9 @@ import
 # 秘密鍵を読み込む
 proc loadPrivateKey*(): string =
   let f: File = open(getAppDir() / "key/private.key", fmRead)
-  defer:
-    f.close()
-    DebugLogging("INFO", "loadPrivateKey", "Loaded private key.")
-  return f.readAll()
+  result = f.readAll()
+  f.close()
+  DebugLogging("INFO", "loadPrivateKey", "Loaded private key.")
 
 # jsonの中に全てのキーが存在するか確認
 proc checkJsonKeys*(jsonBody:JsonNode, keys:seq[string]): bool =
@@ -40,5 +40,23 @@ proc generateJwt*(id:int, deadlineHour:int): string =
     }
   })
   token.sign(private_key)
-  DebugLogging("INFO", "generateJwt", &"Generated token by id '{$id}'")
+  DebugLogging("INFO", "generateJwt", &"Generated token by id '{$id}'.")
   return $token
+
+# jwtでトークン認証
+proc verifyJwt*(token:string): bool =
+  echo token
+  let private_key:string = loadPrivateKey()
+  try:
+    let jwtToken = token.toJwT()
+    result = jwtToken.verify(private_key, HS256)
+    DebugLogging("SUCCESS", "verifyJwt", "Token varification is successful.")
+  except:
+    result = false
+    DebugLogging("ERROR", "verifyJwt", "Token varification is unsuccessful.")
+
+# jwtでトークンからidを取り出し
+proc decodeJwt*(token:string): int =
+  let jwtToken = token.toJWT()
+  result = jwtToken.claims["userId"].node[].num
+  DebugLogging("INFO", "decodeJwt", &"Decoded token -> {$result}")
